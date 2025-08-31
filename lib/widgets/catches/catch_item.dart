1327308e18350cd7af72b76ee17e4ccfb9781efc
lynn2910 <cedric.colin35@gmail.com>
2025-08-31 15:37:59 +0200

    feat(CatchItem): Catch items now have a red background when it's an accident

--- lib/widgets/catches/catch_item.dart
+++ lib/widgets/catches/catch_item.dart
@@ -25,62 +25,82 @@ class _CatchItemState extends State<CatchItem> {
     widget.onCatchEdited();
   }
 
+  Color _getCatchColor(Catch catchData) {
+    if (catchData.accident != null && catchData.accident != Accident.none) {
+      return Colors.red.withValues(alpha: 0.15);
+    }
+    return Colors.transparent;
+  }
+
   @override
   Widget build(BuildContext context) {
     var catchItem = widget.catchItem;
 
     var theme = Theme.of(context);
 
-    return InkWell(
-      onTap: () {
-        showModalBottomSheet<bool>(
-          isScrollControlled: true,
-          context: context,
-          builder: (BuildContext context) {
-            return Wrap(
-              children: [
-                CatchDetails(
-                  catchItem: catchItem,
-                  onCatchDeleted: widget.onCatchDeleted,
-                  onCatchEdited: _onCatchEdited,
-                ),
-              ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 6),
+      child: Container(
+        padding: const EdgeInsets.all(0),
+        decoration: BoxDecoration(
+          color: _getCatchColor(catchItem),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: InkWell(
+          onTap: () {
+            showModalBottomSheet<bool>(
+              isScrollControlled: true,
+              context: context,
+              builder: (BuildContext context) {
+                return Wrap(
+                  children: [
+                    CatchDetails(
+                      catchItem: catchItem,
+                      onCatchDeleted: widget.onCatchDeleted,
+                      onCatchEdited: _onCatchEdited,
+                    ),
+                  ],
+                );
+              },
             );
           },
-        );
-      },
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 12.0),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(width: 8),
-            //
-            // Left
-            //
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "${catchItem.catchDate != null ? _formatDatetime(catchItem.catchDate!) : "-- ERREUR --"} - ${catchItem.fishermenName ?? "-- ERREUR --"}",
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 12.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(width: 8),
+                //
+                // Left
+                //
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "${catchItem.catchDate != null ? _formatDatetime(catchItem.catchDate!) : "-- ERREUR --"} - ${catchItem.fishermenName ?? "-- ERREUR --"}",
+                      ),
+                      Text(getCatchType(catchItem)),
+                    ],
                   ),
-                  Text(getCatchType(catchItem)),
-                ],
-              ),
+                ),
+                //
+                // Right
+                //
+                const SizedBox(width: 8),
+                Text(
+                  _getWeight(catchItem),
+                  style: theme.textTheme.labelMedium!,
+                ),
+                const SizedBox(width: 8),
+                Icon(
+                  Icons.arrow_right,
+                  size: 26,
+                  color: theme.colorScheme.onSurfaceVariant,
+                ),
+              ],
             ),
-            //
-            // Right
-            //
-            const SizedBox(width: 8),
-            Text(_getWeight(catchItem), style: theme.textTheme.labelMedium!),
-            const SizedBox(width: 8),
-            Icon(
-              Icons.arrow_right,
-              size: 26,
-              color: theme.colorScheme.onSurfaceVariant,
-            ),
-          ],
+          ),
         ),
       ),
     );
